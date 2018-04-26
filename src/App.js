4a9bb592eda4caf9c12cc9df83632e3292726f26
Peter Dahlén <peterjandahlen@gmail.com>
2018-04-26 14:05:23 +0200

    Start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Tja!</p>
+        <h1>Yo!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
