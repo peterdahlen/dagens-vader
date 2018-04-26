7e5923497cbca3ce8a9b00f7cacf4fddf5791678
Peter Dahlén <peterjandahlen@gmail.com>
2018-04-26 16:21:11 +0200

    this.setState

--- src/App.js
+++ src/App.js
@@ -6,11 +6,33 @@ import Weather from './components/Weather';
 const API_KEY = "b979265f9415858512000ba26a9c2eb6";
 
 class App extends React.Component {
+  state = {
+    city: undefined,
+    country: undefined,
+    temprature: undefined,
+    description: undefined,
+    humidity: undefined,
+    sunrise: undefined,
+    sunset: undefined, 
+    error: undefined
+  }
   getWeather = async (e) => {
     e.preventDefault();
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&units=metric`);
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
     console.log(data);
+    this.setState({
+      temrature: data.main.temp,
+      city: data.city,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      sunrise: data.sys.sunrise,
+      sunset: data.sys.sunset,
+      error: ""
+    });
   }
   render() {
     return (
